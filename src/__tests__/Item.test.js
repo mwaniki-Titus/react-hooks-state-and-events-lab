a7d0dcd5745a63393170e6a949b9513fb14c046c
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-20 21:01:43 +0300

    completed assungment

--- src/__tests__/Item.test.js
+++ src/__tests__/Item.test.js
@@ -10,6 +10,6 @@ test("the <li> does not have a className when initialized", () => {
 
 test("the <li> has a className of 'in-cart' when the Add to Cart button is clicked", () => {
   const { container } = render(<Item name="Milk" category="Dairy" />);
-  fireEvent.click(screen.getByText(/ Cart/));
+  fireEvent.click(screen.getByText(/Add to Cart/));
   expect(container.querySelector(".in-cart")).toBeInTheDocument();
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+const[isDarkMode, setisDarkMode] = useState(false);
+// Toggle dark mode state when the button is clicked
+  const handleDarkModeToggle = () =>{
+    setisDarkMode(!isDarkMode);
+  };
+  
+  
+  const appClass = isDarkMode? "App dark" : "App light";
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeToggle}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
